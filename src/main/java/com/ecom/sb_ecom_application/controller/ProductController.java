3e02f2e2e08b8fd37dcf12647c769db7f2413197
AjaySharma <ajaysharma@Ajays-MacBook-Air.local>
2025-02-28 00:48:26 +0530

    update file image complete

--- src/main/java/com/ecom/sb_ecom_application/controller/ProductController.java
+++ src/main/java/com/ecom/sb_ecom_application/controller/ProductController.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
@@ -35,5 +38,24 @@ public class ProductController {
     @GetMapping("/public/products/{keyword}")
     public ResponseEntity<ProductResponse> getAllProductsByCategory(@PathVariable String keyword ){
         return new ResponseEntity<>(productService.getProductByKeyword(keyword), HttpStatus.FOUND);
+    }
+
+    @PutMapping("/admin/product/{productId}")
+    public ResponseEntity<ProductDTO> updateProduct(@PathVariable Long productId, @RequestBody Product product){
+        ProductDTO productDTO = productService.updateProduct(productId, product);
+        return new ResponseEntity<>(productDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/admin/products/{productId}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Long productId){
+        productService.deleteProduct(productId);
+        return new ResponseEntity<>("Product deleted", HttpStatus.OK);
+    }
+
+    @PutMapping("/products/{productId}/image")
+    public ResponseEntity<ProductDTO> updateProductImage(@PathVariable Long productId,
+                                                         @RequestParam("image") MultipartFile image) throws IOException {
+        ProductDTO updatedProduct = productService.updateProductImage(productId, image);
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
     }
 }
--- src/main/java/com/ecom/sb_ecom_application/service/ProductService.java
+++ src/main/java/com/ecom/sb_ecom_application/service/ProductService.java
@@ -3,10 +3,17 @@ package com.ecom.sb_ecom_application.service;
 import com.ecom.sb_ecom_application.model.Product;
 import com.ecom.sb_ecom_application.payload.ProductDTO;
 import com.ecom.sb_ecom_application.payload.ProductResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface ProductService {
     ProductDTO addProduct(Long categoryId, Product product);
     ProductResponse getProducts();
     ProductResponse getProductByCategory(Long categoryId);
     ProductResponse getProductByKeyword(String keyword);
+    ProductDTO updateProduct(Long productId, Product product);
+    String deleteProduct(Long productId);
+
+    ProductDTO updateProductImage(Long productId, MultipartFile image) throws IOException;
 }
